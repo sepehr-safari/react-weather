7e45e585e1b0eecda472d230767a32bfca7cb8ac
himmelio <safari.sepehr@gmail.com>
2022-04-11 22:46:44 +0430

    add constants

---
+++ src/helpers/constants.js
@@ -0,0 +1,3 @@
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '305ee0c46ad6728b64d53f697440b436';
+export const UNSPLASH_SOURCE_URL = 'https://source.unsplash.com/random/?nature,weather';
